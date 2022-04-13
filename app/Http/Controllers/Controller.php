49d83a8e5d86ef54902b29086a8ba32010053644
jax020 <passer_jax@163.com>
2022-04-13 16:43:06 +0800

    blog

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function test1()
+    {
+        echo "这里phpstorm修改的结果 blog2项目";
+
+    }
 }
