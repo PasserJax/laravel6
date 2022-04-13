7085334ba8ad6cc3a8aabb3727f1d39bfa37f563
jax020 <passer_jax@163.com>
2022-04-13 16:50:47 +0800

    laralve

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+
+    public function test2()
+    {
+        echo "这里phpstorm修改的结果 laravel 项目";
+
+    }
 }
